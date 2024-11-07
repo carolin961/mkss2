ae939a0adee116c36473eb071d596031acb93d1c
carolin.sch <cschumacher@stud.hs-bremen.de>
2024-11-07 11:53:52 +0100

    exercise 2: try to fix http and https issue

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-# Stage 1: Build the application
+# Build the application
 FROM maven:3.8.4-openjdk-17 AS build
 WORKDIR /app
 COPY pom.xml ./
 COPY src ./src
 RUN mvn clean package -DskipTests
 
-# Stage 2: Run the application
+# Run the application
 FROM openjdk:17-jdk-slim
 WORKDIR /app
 COPY --from=build /app/target/*.jar app.jar
--- src/main/java/com/robot/mkss2/RobotServiceApplication.java
+++ src/main/java/com/robot/mkss2/RobotServiceApplication.java
@@ -1,7 +1,11 @@
 package com.robot.mkss2;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 
 @SpringBootApplication
 public class RobotServiceApplication {
